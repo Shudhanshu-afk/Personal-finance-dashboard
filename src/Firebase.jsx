7b763417141e3ba28bdc0b2c77ac88bd1ed27bb7
Shudhanshu-afk <shudhanshu8611@gmail.com>
2024-08-04 12:49:15 +0530

    Update Firebase.jsx

--- src/Firebase.jsx
+++ src/Firebase.jsx
@@ -9,13 +9,7 @@ import {getFirestore} from 'firebase/firestore'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAtmOJpPIyL8TbFhr52AYmtNdF-SfW3IKo",
-  authDomain: "dashboard-93006.firebaseapp.com",
-  projectId: "dashboard-93006",
-  storageBucket: "dashboard-93006.appspot.com",
-  messagingSenderId: "338675934632",
-  appId: "1:338675934632:web:2075c645b79e023c5d699d",
-  measurementId: "G-40P2V087RG"
+  // Your firebase configuraton
 };
 
 // Initialize Firebase
